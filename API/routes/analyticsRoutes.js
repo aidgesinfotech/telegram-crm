f96bcc8c1c189ffd0236a7593b65b582a3c0bb3c
Aidges Infotech <153446364+aidgesinfotech@users.noreply.github.com>
2025-11-10 13:52:46 +0530

    Delete API directory

--- API/routes/analyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AnalyticsController = require('../controllers/analyticsController');
-
-router.get('/global', AnalyticsController.getGlobalSummary);
-router.get('/bot/:botId', AnalyticsController.getBotSummary);
-
-module.exports = router;
--- API/routes/telegramRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const telegramController = require('../controllers/telegramController');
-
-router.post('/webhook/:botId', telegramController.webhook);
-
-module.exports = router;
